08e5f460788e193980d9be9d4f4ea8317a5f1d23
informatica10 <alexteotonio@bioextratus.rede.bioextratus>
2022-10-24 21:56:34 -0300

    Get Form data and handle form submit

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -1,3 +1,6 @@
+import {useState} from 'react';
+
+
 import {Form, ButtonContainer} from './style';
 
 import { Input } from '../Input';
@@ -6,22 +9,50 @@ import {Button} from '../Button';
 
 import FormGroup from '../FormGroup';
 
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 
 export default function ContactForm({ buttonLabel}) {
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [phone, setPhone] = useState("");
+    const [category, setCategory] = useState("");
+
+    function handleSubmit(event) {
+        event.preventDefault();
+    }
     return(
-        <Form>
+        <Form onSubmit={handleSubmit}>
             <FormGroup >
-                <Input placeholder='Nome'/>
+                <Input 
+                    placeholder='Nome'
+                    value={name}
+                    onChange={(event) => setName(event.target.value)}
+                />
             </FormGroup>
             <FormGroup error = "O formato do e-mail é inválido">
-                <Input placeholder='E-mail' error/>
+                <Input 
+                    placeholder='E-mail' error
+                    value={email}
+                    onChange={(event) => setEmail(event.target.value)}
+                />
             </FormGroup>
             <FormGroup>
-                <Input placeholder='Telefone'/>
+                <Input
+                    placeholder='Telefone'
+                    value={phone}
+                    onChange={(event) => setPhone(event.target.value)}
+                />
             </FormGroup>
             <FormGroup>
-                <Select />
+                <Select 
+                    value={category}
+                    onChange={(event) => setCategory(event.target.value)}
+                >
+                    <option value="">Category</option>
+                    <option value="instagram">Instagram</option>
+                    <option value="facebook">Facebook</option>
+                </Select>
             </FormGroup>
 
             <ButtonContainer>
