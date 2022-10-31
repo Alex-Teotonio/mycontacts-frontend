eec2a9323dcae9afd00d2fa00a160f6f5f6164ed
informatica10 <alexteotonio@bioextratus.rede.bioextratus>
2022-10-31 11:40:22 -0300

    Add property noValidate on a form

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -53,7 +53,7 @@ export default function ContactForm({ buttonLabel}) {
     }
     
     return(
-        <Form onSubmit={handleSubmit}>
+        <Form onSubmit={handleSubmit} noValidate>
             <FormGroup error={getErrorMessageByFieldName('name')}>
                 <Input
                     error = {getErrorMessageByFieldName('name')}
