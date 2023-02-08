2c3fa459b7c192c36aeb6afc68734ae7592d3135
informatica10 <alexteotonio@bioextratus.rede.bioextratus>
2023-02-08 11:56:20 -0300

    Component Toast init

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -4,6 +4,7 @@ import theme from '../../assets/styles/themes/default';
 import {Container} from './style';
 import {BrowserRouter} from 'react-router-dom'
 import Routes from '../../Routes';
+import ToastContainer from '../Toast/ToastContainer';
 
 import Header from '../Header';
 // import ContactList from '../ContactList';
@@ -11,6 +12,7 @@ export default function App() {
   return (
     <BrowserRouter>
     <ThemeProvider theme={theme}>
+      <ToastContainer/>
       <CreateGlobalStyle/>
       <Container>
         <Header/>
---
+++ src/components/Toast/ToastContainer/index.js
@@ -0,0 +1,9 @@
+import { Container } from "./style";
+
+export default function ToastContainer() {
+  return (
+    <Container>
+      <h1>ToastContainer</h1>
+    </Container>
+  )
+}
