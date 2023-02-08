b9673d81c7d7f01f82a1fa067b1872f7eee22251
informatica10 <alexteotonio@bioextratus.rede.bioextratus>
2023-02-08 16:05:19 -0300

    Create methods to remove events

--- src/services/EventManager.js
+++ src/services/EventManager.js
@@ -1,7 +1,6 @@
 export default class EventManager {
   constructor() {
-    this.listeners = {
-      addtoast: 
+    this.listeners = { 
     }
   }
 
@@ -20,6 +19,16 @@ export default class EventManager {
     this.listeners[event].forEach((listener) => {
       listener(payload)
     })
+  }
+
+  removeListener (event, listenerToRemove) {
+    if(!this.listeners[event]) {
+      return;
+    }
+
+    const filteredListeners = this.listeners[event].filter((f) => f !== listenerToRemove);
+
+    this.listeners[event] = filteredListeners;
   }
 }
 
