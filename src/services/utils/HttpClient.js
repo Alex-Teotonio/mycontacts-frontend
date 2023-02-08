1305f808eeb01f59b2a73c0ac17500eb0f38fe7c
informatica10 <alexteotonio@bioextratus.rede.bioextratus>
2023-02-07 21:55:07 -0300

    Refactoring HttpClient

--- src/services/utils/HttpClient.js
+++ src/services/utils/HttpClient.js
@@ -5,37 +5,30 @@ class HttpClient {
     this.baseUrl = baseUrl
   }
   async get(path) {
-
-    await delay(500);
-    const response = await fetch(`${this.baseUrl}${path}`);
-    const contentType = response.headers.get('Content-Type')
-
-    let body = null;
-    if(contentType.includes('application/json')) {
-      body = await response.json();
-    }
-
-    if(response.ok) {
-      return body;
-    }
-    throw new APIError(response, body);
+    return this.makeRequest(path, {method: 'GET'})
   }
 
   async post(path, payload) {
 
+    return this.makeRequest(path, {
+        method: 'POST',
+        body: payload
+      })
+  }
+
+  async makeRequest(path, options) {
     await delay(500);
 
-    const headers = new Headers({
-      'Content-Type': 'application/json'
-    })
-    const response = await fetch(
-      `${this.baseUrl}${path}`,
-      {
-        method: 'POST',
-        body: JSON.stringify(payload),
-        headers
-      }
-    );
+    const headers = new Headers()
+
+    if(options.body) {
+      headers.append('Content-Type', 'application/json')
+    }
+    const response = await fetch(`${this.baseUrl}${path}`, {
+      method: options.method,
+      body: JSON.stringify(options.body),
+      headers
+    });
     const contentType = response.headers.get('Content-Type')
 
     let body = null;
